98a210fa13bb6dd66936fb01f8eaeebb8a06e1ed
sidan001 <ljl052@foxmail.com>
2016-07-05 17:56:30 +0800

    synchronization

---
+++ src/main/java/com/exercise/essential_classes/concurrency/synchronization/guarded_blocks/Consumer.java
@@ -0,0 +1,30 @@
+/**
+ * @Description: TODO
+ */
+package com.exercise.essential_classes.concurrency.synchronization.guarded_blocks;
+
+import java.util.Random;
+
+public class Consumer implements Runnable {
+	private Drop drop;
+
+	public Consumer(Drop drop) {
+		this.drop = drop;
+	}
+
+	@Override
+	public void run() {
+		Random random = new Random();
+		for (String message = drop.take(); !message.equals("DONE"); message = drop.take()) {
+			
+			String threadName = Thread.currentThread().getName();
+			System.out.format("[%s]MESSAGE RECEIVED: %s%n", threadName, message);
+			
+//			System.out.format("MESSAGE RECEIVED: %s%n", message);
+			try {
+				Thread.sleep(random.nextInt(5000));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
---
+++ src/main/java/com/exercise/essential_classes/concurrency/synchronization/guarded_blocks/ProducerConsumerExample.java
@@ -0,0 +1,11 @@
+package com.exercise.essential_classes.concurrency.synchronization.guarded_blocks;
+
+public class ProducerConsumerExample {
+	public static void main(String[] args) {
+		Drop drop = new Drop();
+		(new Thread(new Producer(drop))).start();
+		(new Thread(new Consumer(drop))).start();
+		//模拟多个消费者，生产者生产完后，最终会只有一个消费者拿到生产结束的标志（DONE），其他的消费者会无限等待生产结束标志。
+//		(new Thread(new Consumer(drop))).start();
+	}
+}
